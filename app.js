19ffc4bf35181c312fc7f9e3bae2217c73a9b6c1
Jingwei Wang <paradox.xiyan@gmail.com>
2024-01-22 15:29:51 +0100

    create the ejs view

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const morgan = require('morgan');
-const path = require('path');
+const path = require('node:path');
 const bodyParser = require('body-parser');
 
 const app = express();
 
 app.set('port',process.env.PORT || 3000);
 
-app.set('views',path.join(__dirname, 'view'));
-app.set('views engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+console.log(path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/',(req,res) => {
-    res.send('Hello, world');
+    res.render('index');
 });
 
 app.listen(app.get('port'),() => {
