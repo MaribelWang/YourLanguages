0555bed0a1524723ee0cd1df9b9c3a825e1293ad
Jingwei Wang <paradox.xiyan@gmail.com>
2024-01-25 15:18:55 +0100

    Save tutors datas to local

--- routers/router.js
+++ routers/router.js
@@ -1,12 +1,25 @@
-const express = require('express');
-const router = express.Router();
+module.exports = (app) =>{
+    
+    let tutors = [];
+    app.locals.tutors = tutors;
 
-router.get('/',(req, res) => {
-    res.render('index',{title:"Your Languages"});
-});
-router.get('/teacher-form',(req,res) => {
-    res.render('teacher-form', {title: "New teacher page"});
-});
+    app.get('/',(req, res) => {
+        res.render('index',{title:"Your Languages"});
+    });
+    app.get('/teacher-form',(req,res) => {
+        res.render('teacher-form', {title: "New teacher page"});
+    });
 
-module.exports = router;
-//??
+    app.post('/teacher-form',(req,res) => {
+        if(!req.body.name || req.body.description){
+            res.send(400).send('The name and description should not be blank');
+        }
+        let newTutor = {
+            name: req.body.name,
+            description: req.body.description,
+            location:new Location()
+        };
+        tutors.push(newTutor);
+        res.redirect('/');
+    });
+}
